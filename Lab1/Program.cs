98347d91311e547c3196a466b8a73cab1f8b7718
Madison Cagle <78162094+mac3141@users.noreply.github.com>
2022-02-08 11:29:17 -0600

    Do insertion

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -17,17 +17,27 @@ namespace Lab1
 
                 for (int i = 0; i < MAX; i++)
                 {
-                    intKeyValuePairs.Add(new KeyValuePair<int, int>(i, i + 42));
+                    intKeyValuePairs.Add(new KeyValuePair<int, int>(i, i + 2));
                 }
 
-                //var dictionaryKeyValueMap = new DictionaryKeyValueMap<int, int>();
-                var bstKeyValueMap = new BinarySearchTreeKeyValueMap<int, int>();
-                //var avlKeyValueMap = new AVLTreeKeyValueMap<int, int>();
-                //var redBlackKeyValueMap = new RedBlackTreeKeyValueMap<int, int>();
+                // Hashtable
+                /*var dictionaryKeyValueMap = new DictionaryKeyValueMap<int, int>();
 
+                Console.WriteLine("DictionaryKeyValueMap");
+                Console.WriteLine("Ordered");
+                CreateKeyValueMap<int, int>(dictionaryKeyValueMap, intKeyValuePairs);
 
+                Console.WriteLine("Unordered");
+                intKeyValuePairs.Shuffle();
+                dictionaryKeyValueMap = new DictionaryKeyValueMap<int, int>();
 
-                //Console.WriteLine("DictionaryKeyValueMap");
+                CreateKeyValueMap<int, int>(dictionaryKeyValueMap, intKeyValuePairs);*/
+
+                // ---------------------------------------------------------------------
+
+                // BST
+                /*var bstKeyValueMap = new BinarySearchTreeKeyValueMap<int, int>();
+
                 Console.WriteLine("BSTKeyValueMap");
                 Console.WriteLine("Ordered");
                 CreateKeyValueMap<int, int>(bstKeyValueMap, intKeyValuePairs);
@@ -36,7 +46,37 @@ namespace Lab1
                 intKeyValuePairs.Shuffle();
                 bstKeyValueMap = new BinarySearchTreeKeyValueMap<int, int>();
 
-                CreateKeyValueMap<int, int>(bstKeyValueMap, intKeyValuePairs);
+                CreateKeyValueMap<int, int>(bstKeyValueMap, intKeyValuePairs);*/
+
+                // ---------------------------------------------------------------------
+
+                // AVL
+                /*var avlKeyValueMap = new AVLTreeKeyValueMap<int, int>();
+
+                Console.WriteLine("AVLKeyValueMap");
+                Console.WriteLine("Ordered");
+                CreateKeyValueMap<int, int>(avlKeyValueMap, intKeyValuePairs);
+
+                Console.WriteLine("Unordered");
+                intKeyValuePairs.Shuffle();
+                avlKeyValueMap = new AVLTreeKeyValueMap<int, int>();
+
+                CreateKeyValueMap<int, int>(avlKeyValueMap, intKeyValuePairs);*/
+
+                // ---------------------------------------------------------------------
+
+                // RedBlack
+                /*var redBlackKeyValueMap = new RedBlackTreeKeyValueMap<int, int>();
+
+                Console.WriteLine("RedBlackKeyValueMap");
+                Console.WriteLine("Ordered");
+                CreateKeyValueMap<int, int>(redBlackKeyValueMap, intKeyValuePairs);
+
+                Console.WriteLine("Unordered");
+                intKeyValuePairs.Shuffle();
+                redBlackKeyValueMap = new RedBlackTreeKeyValueMap<int, int>();
+
+                CreateKeyValueMap<int, int>(redBlackKeyValueMap, intKeyValuePairs);*/
             }
 
         }
